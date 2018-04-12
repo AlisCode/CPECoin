3e246672dc6d8dd2d59991703faa6cae7e3d7fae
Olivier Pinon <oliv.pinon@gmail.com>
2018-04-12 02:21:51 +0200

    fixed minor errors

--- src/bin/miner.rs
+++ src/bin/miner.rs
@@ -45,7 +45,9 @@ pub fn main() {
     if path.exists() {
         miner.blockchain = cpecoin::blockchain::load_from_file(path_file);
     } else {
-        miner.blockchain.push(Block::genesis());
+        let mut block_genesis = Block::genesis();
+        block_genesis.calc_hash();
+        miner.blockchain.push(block_genesis);
     }
 
     println!("Now mining!");
@@ -53,19 +55,18 @@ pub fn main() {
         // Gets the latest block in the chain
         let (old_proof, index, prev_hash) = {
             let block = miner.blockchain.last().unwrap();
-            (block.proof, block.index, block.prev_hash.clone())
+            (block.proof, block.index, block.hash.clone())
         };
         // Blocks until the proof is found
         let new_proof = proof_of_work(old_proof);
 
-        let mut transactions = get_transactions();
-        transactions.transactions.push(Transaction::new(
+        let mut data = get_transactions();
+        data.transactions.push(Transaction::new(
             "network".into(),
             miner_address.clone(),
             COINS_PER_BLOCK,
         ));
-        let mut new_block =
-            Block::new_from_prev(index, prev_hash, cpecoin::blockchain::get_transactions());
+        let mut new_block = Block::new_from_prev(index, prev_hash, data);
         new_block.proof = new_proof;
         new_block.calc_hash();
         println!("Mined a block with hash : {}", new_block.hash);
